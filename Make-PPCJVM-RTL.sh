e684dea1c7392550334b3d7cd70e2c960f1fba5d
Peter Dell <peter.dell@web.de>
2024-05-09 19:33:59 +0200

    Use portable repository path

--- Make-PPCJVM-RTL.sh
+++ Make-PPCJVM-RTL.sh
@@ -1,5 +1,5 @@
 # Run from bash, e.g. under Cygwin
-export REPOSITORY_FOLDER="/cygdrive/c/jac/system/Windows/Programming/Repositories"
+export REPOSITORY_FOLDER="$PWD/.."
 export FPC_FOLDER="$REPOSITORY_FOLDER/fpc"
 export FPC_PPCJVM_FOLDER="$REPOSITORY_FOLDER/fpc-ppcjvm"
 export PATH="$FPC_FOLDER/compiler:$FPC_PPCJVM_FOLDER/build:$PATH"
--- Make-PPCJVM.sh
+++ Make-PPCJVM.sh
@@ -1,5 +1,10 @@
 # Run from bash, e.g. under Cygwin
 # https://wiki.freepascal.org/FPC_JVM/Building
-pushd ../fpc
-make OPT="-gl" CROSSOPT="-O2 -gl" CPU_TARGET="jvm" OS_TARGET="java" INSTALL_PREFIX="C:\jac\system\Windows\Programming\Repositories\fpc\out" all crossinstall
+
+export REPOSITORY_FOLDER="$PWD/.."
+export FPC_FOLDER="$REPOSITORY_FOLDER/fpc"
+export FPC_PPCJVM_FOLDER="$REPOSITORY_FOLDER/fpc-ppcjvm"
+export PATH="$FPC_FOLDER/compiler:$FPC_PPCJVM_FOLDER/build:$PATH"
+pushd $FPC_FOLDER
+make OPT="-gl" CROSSOPT="-O2 -gl" CPU_TARGET="jvm" OS_TARGET="java" INSTALL_PREFIX="out" all crossinstall
 popd
